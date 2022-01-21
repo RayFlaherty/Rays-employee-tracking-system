26c94c4ce5602e981c554fcaa4e0dc3da0100ef6
RayFlaherty <90649331+RayFlaherty@users.noreply.github.com>
2022-01-21 16:28:26 -0600

    starter

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+--  DROP TABLE IF EXISTS employee_data;
+ 
+--  CREATE TABLE employee_data (
+--      id INTEGER AUTO_INCREMENT PRIMARY KEY,
+--      department_name VARCHAR(30) NOT NULL
+--      );
